af66d752d133bab0f3f8cb25de466bddc95e9ef9
TommiWindows <tommi.kohonen21@gmail.com>
2025-11-03 12:38:11 +0200

    Lisätty testi laskimeen

--- Viikko1/riippuvuuksien-injektointi/src/tests/laskin_test.py
+++ Viikko1/riippuvuuksien-injektointi/src/tests/laskin_test.py
@@ -21,3 +21,11 @@ class TestLaskin(unittest.TestCase):
         laskin.suorita()
 
         self.assertEqual(io.outputs[0], "Summa: 4")
+
+    def test_kaksi_summa_oikein(self):
+        io = StubIO(["1", "3", "2", "6", "-9999"])
+        laskin = Laskin(io)
+        laskin.suorita()
+
+        self.assertEqual(io.outputs[0], "Summa: 4")
+        self.assertEqual(io.outputs[1], "Summa: 8")

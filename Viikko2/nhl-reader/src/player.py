cd1b515e4c3b028f05f12facba4d901a7b3227e1
TommiWindows <tommi.kohonen21@gmail.com>
2025-11-10 19:55:15 +0200

    Viikko 2 nhl ekat tehtävät

---
+++ Viikko2/nhl-reader/src/player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, dict):
+        self.name = dict['name']
+        self.nationality = dict['nationality']
+        self.assists = dict['assists']
+        self.goals = dict['goals']
+        self.team = dict['team']
+        self.games = dict['games']
+        self.id = dict['id']
+        self.score = (int(dict['goals']) + int(dict['assists']))
+    
+    def __str__(self):
+        return self.name

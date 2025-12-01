c77be0dce1ae19a2e7f02d592b65df0eecb68466
TommiWindows <tommi.kohonen21@gmail.com>
2025-12-01 09:42:10 +0200

    Refactored tennis_game

--- Viikko5/tennis/src/tennis_game.py
+++ Viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,49 @@
 class TennisGame:
+    # Class Constants
+    POINT_NAMES = ["Love", "Fifteen", "Thirty", "Forty"]
+    DEUCE_THRESHOLD = 3  # The score (3 points) at which Deuce logic begins
+    
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.p1_score = 0
+        self.p2_score = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.p1_score += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.p2_score += 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+        if self.p1_score == self.p2_score:
+            return self._get_tied_score()
+        elif self.p1_score > self.DEUCE_THRESHOLD or self.p2_score > self.DEUCE_THRESHOLD:
+            return self._get_end_game_score()
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            return self._get_regular_score()
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+    # Helper Methods
 
-        return score
+    def _get_tied_score(self):
+        if self.p1_score < self.DEUCE_THRESHOLD:
+            # Returns "Love-All", "Fifteen-All", or "Thirty-All"
+            return f"{self.POINT_NAMES[self.p1_score]}-All"
+        return "Deuce"
+
+    def _get_end_game_score(self):
+        score_difference = self.p1_score - self.p2_score
+
+        if score_difference == 1:
+            return f"Advantage {self.player1_name}"
+        elif score_difference == -1:
+            return f"Advantage {self.player2_name}"
+        elif score_difference >= 2:
+            return f"Win for {self.player1_name}"
+        else:
+            return f"Win for {self.player2_name}"
+
+    def _get_regular_score(self):
+        p1_term = self.POINT_NAMES[self.p1_score]
+        p2_term = self.POINT_NAMES[self.p2_score]
+        return f"{p1_term}-{p2_term}"
